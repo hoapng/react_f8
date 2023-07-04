1104fa57a52dc41f6885beb6675b2bd22980412f
hoapng <giahoa01giathieu@gmail.com>
2023-07-04 20:30:39 +0700

    increase

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import { useState } from "react";
 
-const courses = [
-  {
-    id: 1,
-    name: "HTML,CSS"
-  },
-  {
-    id: 2,
-    name: "JS"
-  },
-  {
-    id: 3,
-    name: "ReactJS"
-  }
-]
 function App() {
-  const [checked, setchecked] = useState([])
-  console.log(checked)
-  const handleSubmit = ()=>{
-    //call api
-    console.log({ id: checked})
+  const [couter, setCouter] = useState(1)
+  const handleIncrease =()=>{
+    setCouter(couter+1);
   }
-  const handelchecked = (id)=>{
-    setchecked(prev =>{
-    const isChecked = checked.includes(id)
-    if(isChecked){
-      return checked.filter(item => item !== id)
-    }else {
-      return [...prev, id]
-    }})
-  }
+
   return (
     <>
     <div>
-      {courses.map(courses => (
-        <div key={courses.id}>
-          <input type="checkbox" checked={checked.includes(courses.id)} onChange={()=>handelchecked(courses.id)}/>{courses.name}
-        </div>
-      ))}
-      <button onClick={handleSubmit}>DK</button>
+      <h1>{couter}</h1>
+      <button onClick={handleIncrease}>Tang</button>
     </div>
     </>
   );
