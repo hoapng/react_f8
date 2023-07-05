5dc46256e6a6624d482f3f688d08e62c7202185b
hoapng <giahoa01giathieu@gmail.com>
2023-07-05 07:22:26 +0700

    useEffect hook

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 function Content (){
-    const [title, setTitle] = useState('')
+    // const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
     useEffect(()=>{
-        
-        document.title = title
-    })
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res=>res.json())
+        .then(posts => {
+            setPosts(posts)
+        })
+    },[])
     return(
         <>
-        <input 
+        {/* <input 
         value={title}
-        onChange={e=>setTitle(e.target.value)}/>
-        
+        onChange={e=>setTitle(e.target.value)}/> */}
+        <ul>
+            {posts.map(i =>(<li key={i.id}>{i.title}</li>))}
+        </ul>
         </>
     )
 }
