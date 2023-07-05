a0fe1e8c67aa43a0a86c874f5a6355a4793761cc
hoapng <giahoa01giathieu@gmail.com>
2023-07-05 11:39:46 +0700

    useEffect with fake Chat App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//fake cmt
+function emitComment(id){
+  setInterval(()=>{
+    window.dispatchEvent(
+      new CustomEvent(`lesson-${id}`,{
+        detail: `ND cmt lesson ${id}`
+      })
+    )}, 2000)
+}
+emitComment(1)
+emitComment(2)
+emitComment(3)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
