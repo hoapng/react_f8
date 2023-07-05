2ded2a971bc3eb5e2ebb48aebc7e1f6aab15c528
hoapng <giahoa01giathieu@gmail.com>
2023-07-05 15:57:43 +0700

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import { memo, useCallback, useState } from "react";
-import Content from "./Content";
+import { useState, useRef, useMemo } from "react";
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [products, setProducts] = useState([]);
+  const nameRef = useRef()
   
-  const increase = useCallback(()=>{
-    setCount(pre => pre +1)
-  },[])
+  const handleSubmit = () => {
+    setProducts([...products,{
+      name, 
+      price: +price
+    }])
+    setName('')
+    setPrice('')
+
+    nameRef.current.focus()
+  };
+  const total = useMemo(() => {
+    const result = products.reduce((result, prod) => {
+      console.log('Tinh')
+      return result + prod.price},0)
+      return result
+  },[products])
   
   return (
-    <>
     <div>
-      <Content onIncrease={increase}></Content>
-      <h1>{count}</h1>
-      
-      
+      <input
+        ref={nameRef}
+        placeholder="Enter Name ..."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input
+        placeholder="Enter Price ..."
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <br />
+      <button onClick={handleSubmit}>Add</button>
+      <br />
+      Total: {total}
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
-    </>
   );
 }
 
-export default memo(App);
+export default (App);
