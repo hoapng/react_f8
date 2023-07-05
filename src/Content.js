917798caf210640cc48cef4c587993d1b518686f
hoapng <giahoa01giathieu@gmail.com>
2023-07-05 13:13:49 +0700

    useLayoutEffect hook

--- src/Content.js
+++ src/Content.js
@@ -1,53 +1,20 @@
-import { useState, useEffect } from "react";
-
-const lessons = [
-    {
-      id: 1,
-      name: 'ReactJS là gì? Tại sao nên học ReactJS',
-    },
-    {
-      id: 2,
-      name: 'SPA/MPA là gì ?',
-    },
-    {
-      id: 3,
-      name: 'Arrow function',
-    },
-  ];
+import { useState, useEffect, useLayoutEffect } from "react";
 
 function Content (){
-    const [lessonId, setLessonId] = useState(1);
+  const [count, setScount] = useState(0);
+  const handleRun = () => {
+    setScount(count + 1);
+  };
 
-    useEffect(()=>{
-        const handleCmt = ({detail})=>{
-            console.log({detail});
-        }
-        window.addEventListener(`lesson-${lessonId}`, handleCmt)
-        return ()=>{
-          window.removeEventListener(`lesson-${lessonId}`, handleCmt)
-        }        
-    },[lessonId])
+    useLayoutEffect(()=>{
+      if(count > 3) setScount(0)
+    },[count])
     
     return (
-        <div>
-          <ul>
-            {lessons.map(lesson => {
-                return (
-                    <li
-                        key={lesson.id}
-                        style={{
-                            color: lessonId === lesson.id ? 'red' : '#333'
-                        }}
-                        onClick={() => setLessonId(lesson.id)}
-                    >                    
-                    {lesson.name}
-                    </li>
-                )
-            })
-    
-            }
-          </ul>
-        </div>
+      <div className="App">
+      <h1>{count}</h1>
+      <button onClick={handleRun}>Click</button>
+    </div>
       );
 }
 export default Content
