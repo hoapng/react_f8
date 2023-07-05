baa3505336177c66c180ffd707a81f115811c66a
hoapng <giahoa01giathieu@gmail.com>
2023-07-05 08:44:29 +0700

    useEffect resize

--- src/Content.js
+++ src/Content.js
@@ -1,56 +1,23 @@
 import { useState, useEffect } from "react";
-const tabs = ['posts', 'comments', 'albums']
+
 function Content (){
-    // const [title, setTitle] = useState('')
-    const [content, setcontent] = useState([])
-    const [type, setType] = useState('')
-    const [goTop, setGoTop] = useState(false)
-
-    console.log(type)
-    useEffect(()=>{
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        .then(res=>res.json())
-        .then(content => {
-            setcontent(content)
-        })
-    },[type])
+    const [width, setWidth] = useState(window.innerWidth)
 
     useEffect(()=>{
-        const handleScroll = ()=>{
-            console.log(window.scrollY)
-            if (window.scrollY>=200) setGoTop(true)
-            else setGoTop(false)
+        const handleResize = ()=>{
+            setWidth(window.innerWidth)
         }
-        window.addEventListener('scroll', handleScroll)
+        window.addEventListener('resize', handleResize)
         console.log('addEventListener')
         //Cleanup function
         return ()=>{
-            window.removeEventListener('scroll', handleScroll)
+            window.removeEventListener('scroll', handleResize)
             console.log('removeEventListener')
         }
     },[])
     return(
         <>
-        {tabs.map(tab => (
-            <button 
-            key={tab}
-            style={type === tab ? {backgroundColor: "yellow"} : {}}
-            onClick={() => setType(tab)}>
-                {tab}
-            </button>
-        ))}
-        <ul>
-            {content.map(i =>(<li key={i.id}>{i.title || i.name}</li>))}
-        </ul>
-        {goTop && (
-            <button
-            style={
-                {position: 'fixed',
-                right: 20,
-                bottom: 20}
-            }
-            >Go Top</button>
-        )}
+        <h1>{width}</h1>
         </>
     )
 }
