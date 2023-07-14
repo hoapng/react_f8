4751f9fc6b25e7a59aa8ed80e1b27f0db9f7d761
hoapng <giahoa01giathieu@gmail.com>
2023-07-14 17:01:05 +0700

    customize-cra

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import New from "./pages/New";
-import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/new">New</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      Hello
     </div>
   );
 }
