a918fa0ebf6a69ca1c927145af6442934b03ce55
hoapng <giahoa01giathieu@gmail.com>
2023-07-15 11:04:08 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,12 @@
+import classNames from 'classnames/bind';
+import styles from './header.module.scss';
 function Header() {
+    const cx = classNames.bind(styles);
     return (
         <>
-            <h2>Header</h2>
+            <header className={cx('wrapper')}>
+                <div className={cx('inner')}></div>
+            </header>
         </>
     );
 }
